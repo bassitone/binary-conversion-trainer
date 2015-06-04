8327c5822d88f8a92fba5a3e75562dbe11f4fdf9
bassitone <bassitone@gmail.com>
2015-06-04 00:07:46 -0500

    Debugged the shit out of getEnglishTranslation.  Now to fix the formatting for convertToDecimal.
    Oh yeah, also added a bunch of logic for iterating through examples and regex-y stuff.

--- BinaryConversionTrainer.java
+++ BinaryConversionTrainer.java
@@ -14,42 +14,150 @@ import java.util.ArrayList;
 public class BinaryConversionTrainer
 {
 
-  public static void main(String [] args)
-  {
-
-    //declare all the things
-
-    Scanner input = new Scanner(System.in);
-    char toContinue = 'Y';
-    char binaryChoice = ' ';
-    char sample = ' ';
-    int hasAscii = 3;
-    String answer = "";
-    String decimalResult = "";
-
-
-
-
-    do
+    public static void main(String [] args)
     {
-      //decide if we want to go with sample inputs from the spec
-      System.out.printf("Would you like to work on a sample exercise?%n");
-      sample = input.nextLine().charAt(0);
 
-      //we do
-      if(Character.toUpperCase(sample) == 'Y')
-      {
+        //declare all the things
 
-        System.out.printf("%nOkay, we'll work on a sample problem.  Would you like to start with a value already in binary?");
-        binaryChoice = input.nextLine().charAt(0);
+        Scanner input = new Scanner(System.in);
+        char toContinue = 'Y';
+        char binaryChoice = ' ';
+        char sample = ' ';
+        int hasAscii = 3;
+        int sampleCounter = 0;
+        char continueTo = ' ';
 
-        //yes, binary to decimal/ASCII
-        if(Character.toUpperCase(binaryChoice) == 'Y')
+        String answer = "";
+        String activeExercise = "";
+        String decimalResult = "";
+        Converter aConverter = new Converter();
+
+
+
+        do
         {
+            //decide if we want to go with sample inputs from the spec
+            System.out.printf("Would you like to work on a sample exercise?%n");
+            sample = input.nextLine().charAt(0);
 
-          //prompt user to get an ASCII conversion table
-          while(hasAscii !=0)
-          {
+            //we do
+            if(Character.toUpperCase(sample) == 'Y')
+            {
+
+                System.out.printf("%nOkay, we'll work on a sample problem.  Would you like to start " +
+                        "with a value already in binary?");
+                binaryChoice = input.nextLine().charAt(0);
+
+                //yes, binary to decimal/ASCII
+                if(Character.toUpperCase(binaryChoice) == 'Y')
+                {
+
+                    //prompt user to get an ASCII conversion table
+                    hasAscii(hasAscii);
+                    //end Ascii check
+
+                    System.out.printf("%nAlright, here we go.%nEach of the following sets of 8 bits" +
+                            " represents a character in the ASCII character set.%n" +
+                            "Convert each byte to its decimal equivalent,%n" +
+                            "then use those numbers from 0 to 255 "
+                            + "to find the corresponding ASCII character.");
+
+                    do
+                    {
+
+                        activeExercise = aConverter.getBinaryExercise(sampleCounter);
+
+                        System.out.printf("%nHere's your challenge: %s%nPlease enter your answer here:",
+                                activeExercise);
+
+                        String activeExerciseHelper = "";
+
+                        for(int index = 0; index < activeExercise.length(); index++)
+                        {
+                            activeExerciseHelper = activeExercise.replaceAll("[,\\[\\] ]", "");
+                        }
+
+                        answer = input.nextLine();
+
+                        decimalResult = aConverter.convertToDecimal(activeExerciseHelper);
+
+                        System.out.println(decimalResult);
+
+                        System.out.printf("The English translation of those values is: %s",
+                                aConverter.getEnglishTranslation(decimalResult));
+
+                        //more exercises?
+                        boolean valid = false;
+
+                        while(!valid)
+                        {
+
+                            System.out.printf("%nWould you like to try another exercise?");
+                            continueTo = input.nextLine().charAt(0);
+
+                            switch (Character.toUpperCase(continueTo))
+                            {
+                                case 'Y':
+                                    sampleCounter++;
+                                    valid = true;
+                                    break;
+                                case 'N':
+                                    sampleCounter += 3;
+                                    valid = true;
+                                    break;
+                                default:
+                                    System.out.printf("%nSorry.  Please enter 'yes' or 'no' and try again.%n");
+                                    valid = false;
+                            }
+                        }
+
+                    } while(sampleCounter < 3);
+
+
+                    //System.out.printf("DEBUG: helloWorldBinary: %s", helloWorldBinary);
+
+                }//end if
+
+                else //going TO binary from decimal/ASCII
+                {
+
+
+
+                }//end else
+
+            }
+
+            //decide whether we are going to or from binary
+            System.out.printf("Are you starting with a value already in binary?%n");
+            binaryChoice = input.nextLine().charAt(0);
+
+            //yes, binary to decimal/ASCII
+            if(Character.toUpperCase(binaryChoice) == 'Y')
+            {
+
+
+            }//end if
+
+            else //going TO binary from decimal/ASCII
+            {
+
+            }//end else
+
+            System.out.printf("%n Would you like to continue?");
+            toContinue = input.nextLine().charAt(0);
+
+        }
+        while(Character.toUpperCase(toContinue) == 'Y');
+        //end main do/while
+
+        System.exit(0);
+
+    }//end main()
+
+    public static void hasAscii(int hasAscii)
+    {
+        while(hasAscii !=0)
+        {
             hasAscii = JOptionPane.showConfirmDialog(null, "Alright, you will need an ASCII table for this.\nGo ahead and Google one, I can wait.\nPlease let me know when you are ready");
 
             //System.out.printf("%nDEBUG: n = %d%n", hasAscii);
@@ -58,62 +166,11 @@ public class BinaryConversionTrainer
             //if user cancels, exit program
             if(hasAscii == 2)
             {
-              JOptionPane.showMessageDialog(null, "Alright then.  Goodbye!");
-              System.exit(0);
+                JOptionPane.showMessageDialog(null, "Alright then.  Goodbye!");
+                System.exit(0);
             }
 
-          } //end Ascii check
-
-          System.out.printf("%nAlright, here we go.%nEach of the following sets of 8 bits represents a character in the ASCII character set.%nConvert each byte to its decimal equivalent,%nthen use those numbers from 0 to 255 "
-                              + "to find the corresponding ASCII character.");
-          //System.out.printf("%nHere's your first challenge: %s%nPlease enter your answer here:", helloWorldBinary);
-
-          answer = input.nextLine();
-
-          //decimalResult = binaryToDecimal(helloWorldBinary);
-
-          System.out.print(decimalResult);
-
-
-          //System.out.printf("DEBUG: helloWorldBinary: %s", helloWorldBinary);
-
-        }//end if
-
-        else //going TO binary from decimal/ASCII
-        {
-
-
-
-        }//end else
-
-      }
-
-      //decide whether we are going to or from binary
-      System.out.printf("Are you starting with a value already in binary?%n");
-      binaryChoice = input.nextLine().charAt(0);
-
-      //yes, binary to decimal/ASCII
-      if(Character.toUpperCase(binaryChoice) == 'Y')
-      {
-
-
-      }//end if
-
-      else //going TO binary from decimal/ASCII
-      {
-
-      }//end else
-
-      System.out.printf("%n Would you like to continue?");
-      toContinue = input.nextLine().charAt(0);
-
+        }
     }
-    while(Character.toUpperCase(toContinue) == 'Y');
-    //end main do/while
-
-    System.exit(0);
-
-  }//end main()
-
-
 }//end application class BinaryConversionTrainer
+
--- Converter.java
+++ Converter.java
@@ -1,4 +1,5 @@
 import javax.script.ScriptEngine;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -7,24 +8,24 @@ import java.util.Random;
  *
  * This class does the processing of binary or decimal inputs to their opposite
  */
-public class Converter
-{
+public class Converter {
     //sample problems
-    private static final String helloWorldBinary = "01001000"+"01100101"+"01101100"+"01101100"+"01101111"+"00100000"+"01010111"+"01101111"+"01110010"+"01101100"+"01100100";
+    private static final String helloWorldBinary = "01001000" + "01100101" + "01101100" + "01101100" +
+            "01101111" + "00100000" + "01010111" + "01101111" + "01110010" + "01101100" + "01100100";
 
-    private static final String testBinaryOne = "01110000 01101100 01100101 01100001 01110011 "
-            + "01100101 00100000 01110100 "
-            + "01100001 01101100 01101011 "
-            + "00100000 01110100 01101111 "
-            + "00100000 01101101 01100101";
+    private static final String testBinaryOne = "01110000" + "01101100" + "01100101" + "01100001" + "01110011"
+            + "01100101" + "00100000" + "01110100"
+            + "01100001" + "01101100" + "01101011"
+            + "00100000" + "01110100" + "01101111"
+            + "00100000" + "01101101" + "01100101";
 
-    private static final String testBinaryTwo = "01101100 01101001 01100110 01100101 00100000 "
-            + "01110010 01101001 01100111 "
-            + "01101000 01110100 00100000 "
-            + "01101110 01101111 01110111 "
-            + "00100000 01101001 01110011 "
-            + "00100000 01101100 01101111 "
-            + "01101110 01100101 01101100 01111001";
+    private static final String testBinaryTwo = "01101100" + "01101001" + "01100110" + "01100101" + "00100000"
+            + "01110010" + "01101001" + "01100111"
+            + "01101000" + "01110100" + "00100000"
+            + "01101110" + "01101111" + "01110111"
+            + "00100000" + "01101001" + "01110011"
+            + "00100000" + "01101100" + "01101111"
+            + "01101110" + "01100101" + "01101100" + "01111001";
 
     //instance variables
     private Random ipAddressGenerator = new Random();
@@ -34,22 +35,18 @@ public class Converter
     private int fourthOctet = 0;
 
 
-
-    public Converter()
-    {
+    public Converter() {
         //default constructor, may do ~things~ with it
 
     }
 
-    public Converter( int placeHolder)
-    {
+    public Converter(int placeHolder) {
 
     }
 
 
     //binary to decimal
-    public String convertToDecimal(String s)
-    {
+    public String convertToDecimal(String s) {
         //arraylist to handle any number of characters.
 
         int length = s.length();
@@ -60,13 +57,12 @@ public class Converter
      * ArrayList provides scalability
      */
 
-        ArrayList <String> substrings = new ArrayList <>();
-        ArrayList <String> decimals = new ArrayList<>();
+        ArrayList<String> substrings = new ArrayList<>();
+        ArrayList<String> decimals = new ArrayList<>();
 
-        for(int placeHolder = 0; placeHolder < length; placeHolder+=8)
-        {
+        for (int placeHolder = 0; placeHolder < length; placeHolder += 8) {
             int index = 0;
-            substrings.add(index, s.substring(placeHolder, placeHolder+8));
+            substrings.add(index, s.substring(placeHolder, placeHolder + 8));
 
 
             index++;
@@ -91,57 +87,119 @@ public class Converter
         takes the result from convertToDecimal and turns them into their character values.
         Do not call me for practicing.  Real world use is fine though.
      */
-    public String getEnglishTranslation(String binaryCharacters)
-    {
+    public String getEnglishTranslation(String binaryCharacters) {
         //stores the decimal characters passed to this method
         ArrayList<String> charString = new ArrayList<>();
         /*
             is using an ArrayList overkill?  Maybe a regular array will suffice
         */
         ArrayList<Integer> charValues;
+        char [] castOfCharacters;
+
+        //Debug: System.out.println("binaryCharacters: " + binaryCharacters);
+
+        int begin = 0;
 
 
-        for(int index = 0; index < binaryCharacters.length(); index++)
-        {
+        for (int index = 0; index < binaryCharacters.length(); index++) {
             //This loop looks at binaryCharacters and extracts the decimal strings
-            int begin = 0;
 
-            if(binaryCharacters.charAt(index) == '.')
+
+            if (binaryCharacters.charAt(index) == '.')
             {
-                charString.add(binaryCharacters.substring(begin, index - 1));
+                charString.add(binaryCharacters.substring(begin, index));
                 begin = index + 1;
             }
         }
 
+        //Debug: System.out.println("charString: " + charString);
+
         charValues = new ArrayList<>(charString.size());
 
-        for(String value : charString)
+        for (int val = 0; val < charString.size(); val++)
         {
             //change the strings in charString to integers
-            charValues.add(Integer.getInteger(value));
+            charValues.add(Integer.parseInt(charString.get(val)));
         }
 
-        ArrayList<Character> castOfCharacters = new ArrayList<>(charValues.size());
+        //Debug: System.out.println("charValues: " + charValues + "\n size: " + charValues.size());
 
-        for (Integer charValue : charValues)
+        castOfCharacters = new char[charString.size()];
+
+        //Debug: System.out.println(castOfCharacters.length);
+
+        int convertHelper;
+        for (int charValue = 0; charValue < castOfCharacters.length; charValue++)
         {
             //change the integers to their character values
-            int convertHelper = charValue;
+            convertHelper = charValues.get(charValue);
+            //Debug: System.out.println("convertHelper: " + convertHelper);
             char aCharacter = (char) convertHelper;
-            castOfCharacters.add(aCharacter);
+            castOfCharacters[charValue] = aCharacter;
         }
+
+        //Debug: System.out.println(castOfCharacters);
 
         String englishResult = "";
 
-        for(Character actor : castOfCharacters)
+        for (int charCount = castOfCharacters.length - 1; charCount >= 0; charCount--)
         {
             //and unify the characters in a String to send back.
-            englishResult += actor;
+            englishResult += castOfCharacters[charCount];
         }
 
-
+        //System.out.println(englishResult);
 
         return englishResult;
+
+    }
+
+
+    public String getBinaryExercise(int sampleCount)
+    {
+        ArrayList<String> substrings = new ArrayList<>();
+        int begin = 0;
+        String example = "";
+        switch (sampleCount) {
+            case 0:
+                for(int loc = 0; loc < helloWorldBinary.length(); loc++)
+                {
+                    if(loc % 8 == 0 && loc != 0)
+                    {
+                        substrings.add(helloWorldBinary.substring(begin, loc));
+                        //substrings.add(" ");
+                        begin = loc + 1;
+                    }
+                }
+                example = substrings.toString();
+                break;
+            case 1:
+                for(int loc = 0; loc < testBinaryOne.length(); loc++)
+                {
+                    if(loc % 8 == 0)
+                    {
+                        substrings.add(testBinaryOne.substring(begin, loc));
+                        //substrings.add(" ");
+                        begin = loc + 1;
+                    }
+                }
+                example = substrings.toString();
+                break;
+            case 2:
+                for(int loc = 0; loc < testBinaryTwo.length(); loc++)
+                {
+                    if(loc % 8 == 0)
+                    {
+                        substrings.add(testBinaryTwo.substring(begin, loc));
+                        //substrings.add(" ");
+                        begin = loc + 1;
+                    }
+                }
+                example = substrings.toString();
+                break;
+        }
+
+        return example;
 
     }
 }
